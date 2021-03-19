bdf24cb38d12c5e796184e071cc0277761a06758
juniorplay7529 <37526431+juniorplay7529@users.noreply.github.com>
2021-03-18 21:42:08 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,53 +1,51 @@
 let canvas;
 let ctx;
 
-window.onload = function(){
+window.onload = function () {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
 
     document.addEventListener("keydown", keyDownEvent);
 
-    //Renderiza 8 vezes por segundo
     let x = 8;
     setInterval(desenharJogo, 1000 / x);
 };
-    
 
-//Criação da tela de jogo
+
+
 let tamanhoTela = tamanhoCaminho = 20;
 let nextX = nextY = 0;
 
-//Criação da cobra
+
 let defaultTamanhoCauda = 3;
 let tamanhoCauda = defaultTamanhoCauda;
 let caminhoCobra = [];
 let cobraEixoX = cobraEixoY = 10;
 
-//Criação da comida
 let appleX = (appleY = 15);
 
-function desenharJogo(){
+function desenharJogo() {
     cobraEixoX += nextX;
     cobraEixoY += nextY;
 
-    if (cobraEixoX < 0){
-        cobraEixoX = tamanhoTela -1;
+    if (cobraEixoX < 0) {
+        cobraEixoX = tamanhoTela - 1;
     }
-    
-    if (cobraEixoX > tamanhoTela - 1){
+
+    if (cobraEixoX > tamanhoTela - 1) {
         cobraEixoX = 0;
     }
-    
-    if (cobraEixoY < 0){
-        cobraEixoY = tamanhoTela -1;
+
+    if (cobraEixoY < 0) {
+        cobraEixoY = tamanhoTela - 1;
     }
-    
-    if (cobraEixoY > tamanhoTela - 1){
+
+    if (cobraEixoY > tamanhoTela - 1) {
         cobraEixoY = 0;
     }
 
-    //Se a cobra comer o alimento
-    if (cobraEixoX == appleX && cobraEixoY == appleY){
+
+    if (cobraEixoX == appleX && cobraEixoY == appleY) {
         tamanhoCauda++;
         appleX = Math.floor(Math.random() * tamanhoTela);
         appleY = Math.floor(Math.random() * tamanhoTela);
@@ -57,14 +55,14 @@ function desenharJogo(){
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.fillStyle = "green";
-    for (let i = 0; i < caminhoCobra.length; i++){
+    for (let i = 0; i < caminhoCobra.length; i++) {
         ctx.fillRect(
             caminhoCobra[i].x * tamanhoCaminho,
             caminhoCobra[i].y * tamanhoCaminho,
             tamanhoCaminho,
             tamanhoCaminho
         );
-        if (caminhoCobra[i].x == cobraEixoX && caminhoCobra[i].y == cobraEixoY){
+        if (caminhoCobra[i].x == cobraEixoX && caminhoCobra[i].y == cobraEixoY) {
             tamanhoCauda = defaultTamanhoCauda;
         }
     }
@@ -73,18 +71,17 @@ function desenharJogo(){
     ctx.fillRect(appleX * tamanhoCaminho, appleY * tamanhoCaminho, tamanhoCaminho, tamanhoCaminho);
 
     caminhoCobra.push({
-        x:cobraEixoX,
-        y:cobraEixoY
+        x: cobraEixoX,
+        y: cobraEixoY
     });
-    while (caminhoCobra.length > tamanhoCauda){
+    while (caminhoCobra.length > tamanhoCauda) {
         caminhoCobra.shift();
     }
 }
 
-function keyDownEvent(event){
-    // nextX e nextY representam as direções que a cobra irá percorrer
-    // nos eixos X e Y, respectivamente
-    switch(event.keyCode){
+function keyDownEvent(event) {
+
+    switch (event.keyCode) {
         case 37:
             nextX = -1;
             nextY = 0;
